e3da9ed5c346fe17ae33dcd80dc56c8656901f2a
Evan Cui <yichen.cui@mail.utoronto.ca>
2021-10-22 19:36:02 -0400

    Write tests for parsing functions (#11)
    * Organize packages and edit testing command
    
    * Create HTML test files
    
    * Add data parser tests
    
    * Update prettier formatter

---
+++ test/cases-axios.ts
@@ -0,0 +1,15 @@
+export default new Map([
+  [
+    "https://cs.utm.utoronto.ca/~cuiyiche/tests/anchor.html",
+    "\n\nThis is a link\n\n\n\n"
+  ],
+  ["https://cs.utm.utoronto.ca/~cuiyiche/tests/empty.html", "\n\n\n\n"],
+  [
+    "https://cs.utm.utoronto.ca/~cuiyiche/tests/header.html",
+    "\n\nThis is a header\n\n\n\n"
+  ],
+  [
+    "https://cs.utm.utoronto.ca/~cuiyiche/tests/paragraph.html",
+    "\n\nThis is a paragraph\n\n\n\n"
+  ]
+]);
---
+++ test/cases-fs.ts
@@ -0,0 +1,6 @@
+export default new Map([
+  ["webserver/public-html/anchor.html", "\n\nThis is a link\n\n\n\n"],
+  ["webserver/public-html/empty.html", "\n\n\n\n"],
+  ["webserver/public-html/header.html", "\n\nThis is a header\n\n\n\n"],
+  ["webserver/public-html/paragraph.html", "\n\nThis is a paragraph\n\n\n\n"]
+]);
