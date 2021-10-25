d0c1e474413e84c11c0a669d70620b7d011ca76d
Myles Thiessen <mthiessen@cs.toronto.edu>
2021-10-24 22:25:03 -0400

    Added container to container communication via a docker bridge (#14)
    * Added container to container communication via a docker bridge. Also added a run script to launch the terminal browser.
    
    * Update test cases and script
    
    * Add usage instructions for run script
    
    Co-authored-by: Yichen Cui <yichen.cui@mail.utoronto.ca>

--- webserver/start-test-webserver.sh
+++ webserver/start-test-webserver.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+docker network create text-browser-network
 docker build -t test-webserver .
-docker run -dit --name test-webserver -p 8080:80 test-webserver
+docker run -dit --name test-webserver --network text-browser-network -p 8080:80 test-webserver
