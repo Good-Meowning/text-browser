c91571080f49de88fd4e2a4e23472178a3652136
Yichen Cui <yichen.cui@mail.utoronto.ca>
2021-10-24 22:07:50 -0400

    Update test cases and script

--- test/cases-axios.ts
+++ test/cases-axios.ts
@@ -1,15 +1,6 @@
 export default new Map([
-  [
-    "https://cs.utm.utoronto.ca/~cuiyiche/tests/anchor.html",
-    "\n\nThis is a link\n\n\n\n"
-  ],
-  ["https://cs.utm.utoronto.ca/~cuiyiche/tests/empty.html", "\n\n\n\n"],
-  [
-    "https://cs.utm.utoronto.ca/~cuiyiche/tests/header.html",
-    "\n\nThis is a header\n\n\n\n"
-  ],
-  [
-    "https://cs.utm.utoronto.ca/~cuiyiche/tests/paragraph.html",
-    "\n\nThis is a paragraph\n\n\n\n"
-  ]
+  ["http://test-webserver/anchor.html", "\n\nThis is a link\n\n\n\n"],
+  ["http://test-webserver/empty.html", "\n\n\n\n"],
+  ["http://test-webserver/header.html", "\n\nThis is a header\n\n\n\n"],
+  ["http://test-webserver/paragraph.html", "\n\nThis is a paragraph\n\n\n\n"]
 ]);
