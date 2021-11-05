17e60f9b5f1fd1f2356df811f84f705e5bc5996f
Yichen Cui <yichen.cui@mail.utoronto.ca>
2021-11-04 23:19:33 -0400

    Implement HTML tags parser

--- src/backend-class/data-parser.ts
+++ src/backend-class/data-parser.ts
@@ -1,5 +1,6 @@
 import * as cheerio from "cheerio";
 import { isComment, isTag, isText } from "domhandler";
+import { escape } from "blessed";
 import { getData } from "backend-class";
 
 /**
@@ -27,25 +28,53 @@ function parseTree(node: cheerio.Node | null): string {
   if (!node || isComment(node)) return "";
 
   // Node is only text
-  if (isText(node)) return node.data;
+  if (isText(node)) return escape(node.data);
 
   // Node is HTML tag
   if (isTag(node)) {
     // Ignore CSS and JavaScript
     if (node.type === "style" || node.type === "script") return "";
 
+    let prefix = "";
+    let postfix = "";
+
+    switch (node.tagName.toLowerCase()) {
+      case "i":
+        prefix = "{inverse}";
+        postfix = "{/inverse}";
+        break;
+
+      case "u":
+        prefix = "{underline}";
+        postfix = "{/underline}";
+        break;
+
+      case "blink":
+        prefix = "{blink}";
+        postfix = "{/blink}";
+        break;
+
+      case "h1":
+      case "h2":
+      case "h3":
+      case "h4":
+      case "h5":
+      case "h6":
+      case "b":
+        prefix = "{bold}";
+        postfix = "{/bold}";
+        break;
+
+      default:
+        break;
+    }
+
     // Traverse tree for text with DFS
     return node.childNodes.reduce(
-      (result, currentNode) => result + parseTree(currentNode),
+      (result, currentNode) =>
+        prefix + result + parseTree(currentNode) + postfix,
       ""
     );
-
-    // TODO: uhhhhh
-    // let result = "";
-    // for (const child of node.childNodes) {
-    //   result += parseTree(child);
-    // }
-    // return result;
   }
 
   // Node is neither text nor HTML tag
